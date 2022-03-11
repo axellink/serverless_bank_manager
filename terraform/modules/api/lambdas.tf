b309eb5fef82d2fb0dc5dd2366a5d657304c4394
Axel Vanzaghi <axellinkgm@gmail.com>
2022-03-12 00:17:32 +0100

    refactoring lambda deployment

--- terraform/modules/api/lambdas.tf
+++ terraform/modules/api/lambdas.tf
@@ -1,6 +1,12 @@
+locals {
+    lambda_names = ["create_db", "hello"]
+}
+
 # First create role for the lambda
-resource "aws_iam_role" "lambda_create_db_role" {
-    name = "lambda_create_db_role"
+resource "aws_iam_role" "lambda_role" {
+    for_each = toset(local.lambda_names)
+
+    name = "lambda_${each.key}_role"
     
 
     assume_role_policy = <<EOF
@@ -21,8 +27,10 @@ EOF
 }
 
 # then create the log policy
-resource "aws_iam_policy" "lambda_create_db_log_policy"{
-    name        = "lambda_create_db_log_policy"
+resource "aws_iam_policy" "lambda_log_policy"{
+    for_each = toset(local.lambda_names)
+
+    name        = "lambda_${each.key}_log_policy"
     path        = "/"
     description = "Policy for a lambda to create log"
 
@@ -41,7 +49,7 @@ resource "aws_iam_policy" "lambda_create_db_log_policy"{
                     "logs:PutLogEvents"
                 ],
                 Resource = [
-                    format("arn:aws:logs:%s:%s:log-group:/aws/lambda/%s:*", var.region, var.account_id, "create_db")
+                    format("arn:aws:logs:%s:%s:log-group:/aws/lambda/%s:*", var.region, var.account_id, each.key)
                 ]
             }
         ]
@@ -49,37 +57,47 @@ resource "aws_iam_policy" "lambda_create_db_log_policy"{
 }
 
 # Finally tie them together
-resource "aws_iam_role_policy_attachment" "lambda_create_db_log_policy_attachment" {
-    role      = aws_iam_role.lambda_create_db_role.name
-    policy_arn = aws_iam_policy.lambda_create_db_log_policy.arn
+resource "aws_iam_role_policy_attachment" "lambda_log_policy_attachment" {
+    for_each = aws_iam_role.lambda_role
+
+    role       = each.value.name
+    policy_arn = aws_iam_policy.lambda_log_policy[each.key].arn
 }
 
 # And we need secret to access db secrets
-resource "aws_iam_role_policy_attachment" "lambda_create_db_access_db_secret_policy_attachment" {
-    role      = aws_iam_role.lambda_create_db_role.name
+resource "aws_iam_role_policy_attachment" "lambda_access_db_secret_policy_attachment" {
+    for_each = aws_iam_role.lambda_role
+
+    role       = each.value.name
     policy_arn = aws_iam_policy.access_db_secret_policy.arn
 }
 
 # And we need permission to create EC2 interface
-resource "aws_iam_role_policy_attachment" "lambda_create_db_EC2_interface_policy_attachment" {
-    role       = aws_iam_role.lambda_create_db_role.name
+resource "aws_iam_role_policy_attachment" "lambda_EC2_interface_policy_attachment" {
+    for_each = aws_iam_role.lambda_role
+
+    role       = each.value.name
     policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
 
 # Zip the source
-data "archive_file" "create_db_zip" {
+data "archive_file" "create_zip" {
+    for_each = toset(local.lambda_names)
+
     type        = "zip"
-    source_dir  = format("%s/API/scripts/create_db", var.local_dir)
-    output_path = format("%s/API/build/create_db.zip", var.local_dir)
+    source_dir  = "${var.local_dir}/API/scripts/${each.key}"
+    output_path = "${var.local_dir}/API/build/${each.key}.zip"
 }
 
 # And then upload it
 resource "aws_lambda_function" "lambda_create_db" {
-    filename      = data.archive_file.create_db_zip.output_path
-    function_name = "create_db"
-    role          = aws_iam_role.lambda_create_db_role.arn
+    for_each = data.archive_file.create_zip
+
+    filename      = each.value.output_path
+    function_name = each.key
+    role          = aws_iam_role.lambda_role[each.key].arn
     runtime       = "python3.8"
-    handler       = "create_db.lambda_handler"
+    handler       = "${each.key}.lambda_handler"
     layers        = [aws_lambda_layer_version.psycopg2.arn, aws_lambda_layer_version.bank_manager_helper.arn]
 
     environment {
@@ -97,10 +115,6 @@ resource "aws_lambda_function" "lambda_create_db" {
 
 # Invoke lambda to create database
 resource "aws_lambda_invocation" "action_create_db" {
-    function_name = aws_lambda_function.lambda_create_db.function_name
+    function_name = "create_db"
     input         = "{}"
-}
-
-output "create_db_out" {
-  value = aws_lambda_invocation.action_create_db.result
 }
---
+++ terraform/modules/api/output.tf
@@ -0,0 +1,3 @@
+output "create_db_out" {
+  value = aws_lambda_invocation.action_create_db.result
+}
